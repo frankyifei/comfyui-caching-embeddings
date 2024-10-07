88072c9df5b071322e0d918fd2a6860832d2906c
Yifei(Frank) ZHU <fredyifei@gmail.com>
2024-10-07 11:45:32 +1030

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from .nodes import CachingCLIPTextEncode
 
 NODE_CLASS_MAPPINGS = {
-    "CachingCLIPTextEncode": CachingCLIPTextEncode,
+    "CLIPTextEncodeWithCache": CLIPTextEncodeWithCache,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "CachingCLIPTextEncode": "CLIP Text Encode with Caching",
+    "CLIPTextEncodeWithCache": "CLIP Text Encode with Caching",
 }
